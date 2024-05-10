117f5d92d27153ec37384f804828de11f1774f76
Rafo Gasparyan <rafik.gasparyan@team.gg>
2024-05-10 11:52:34 +0400

    remove unused code

--- picker/build.gradle.kts
+++ picker/build.gradle.kts
@@ -55,7 +55,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "am.gg"
                 artifactId = "CountryPicker"
-                version = "1.0"
+                version = "1.0.1"
             }
         }
     }
